b7c9a0d4d5e8cf352223d2bb7273c15677f8f9c3
Nicolò Zannini <nikisa97@gmail.com>
2019-11-23 01:53:35 +0100

    HasDeltaRadious fixed

--- Assets/Scripts/SM/BaseBehaviours/BS_BossOrbitState.cs
+++ Assets/Scripts/SM/BaseBehaviours/BS_BossOrbitState.cs
@@ -16,10 +16,11 @@ public class BS_BossOrbitState : BossBaseState
     public override void Tick()
     {
         OrbitTick();
+        Debug.Log(boss.Data.orbit.CenterPoint.transform.localEulerAngles.y);
     }
     public override void Exit()
     {
-        OrbitExit();
+
     }
 
     public void OrbitEnter() {
@@ -28,21 +29,20 @@ public class BS_BossOrbitState : BossBaseState
         boss.Data.orbit.Tentacle.transform.SetParent(boss.Data.orbit.CenterPoint.transform);
         boss.Data.orbit.Tentacle.transform.position = new Vector3(boss.Data.orbit.Tentacle.transform.position.x + boss.Data.orbitInfo.InitialRadius, boss.Data.orbit.Tentacle.transform.position.y, boss.Data.orbit.Tentacle.transform.position.z);
         initialPosition = boss.Data.orbitInfo.InitialRadius;
-        Debug.Log(initialPosition + "qui");
-        
     }
 
     public void OrbitTick() {
         angleRotation = Time.deltaTime * boss.Data.orbitInfo.OrbitSpeed;
         if (boss.Data.orbit.CenterPoint.transform.localEulerAngles.y <= boss.Data.orbitInfo.Angle) {
-            //if (HasDeltaRadius)
-            //{
-            //    currentRadius = CenterPoint.transform.localEulerAngles.y / Angle * 100;
-            //    Tentacle.transform.position += new Vector3(angleRotation/100, Tentacle.transform.position.y, Tentacle.transform.position.z);
-            //    Debug.Log(Tentacle.transform.position.x);
-            //}
-            //Debug.Log(CenterPoint.transform.localEulerAngles.y);
+            
+            if (boss.Data.orbitInfo.HasDeltaRadius) {
+                Debug.LogFormat("currentRotation{0},finalPossition{1},FInalAngle{2}", boss.Data.orbit.CenterPoint.transform.localEulerAngles.y, boss.Data.orbitInfo.FinalRadius, boss.Data.orbitInfo.Angle);
+                boss.Data.orbit.Tentacle.transform.localPosition = new Vector3(toFinalRadious(boss.Data.orbit.CenterPoint.transform.localEulerAngles.y, boss.Data.orbitInfo.FinalRadius, boss.Data.orbitInfo.Angle), boss.Data.orbit.Tentacle.transform.localPosition.y, boss.Data.orbit.Tentacle.transform.localPosition.z);
+
+                Mathf.Clamp(boss.Data.orbit.Tentacle.transform.localPosition.x, initialPosition, boss.Data.orbitInfo.FinalRadius);
+            }
             boss.Data.orbit.CenterPoint.transform.Rotate(Vector3.up * angleRotation);
+
         }
         else {
             animator.SetTrigger("Idle");
@@ -51,5 +51,17 @@ public class BS_BossOrbitState : BossBaseState
 
     public void OrbitExit() {
         
+    }
+
+    float toFinalRadious(float _curreRadious , float _finalPosition , float _angleRotation) {
+        _finalPosition = boss.Data.orbitInfo.FinalRadius - boss.Data.orbitInfo.InitialRadius;
+        float currentPosition = (_curreRadious * _finalPosition) / _angleRotation;
+
+        if (currentRadius < boss.Data.orbitInfo.InitialRadius) {
+            return currentPosition + boss.Data.orbitInfo.InitialRadius;
+        }
+        else {
+            return currentPosition;
+        }
     }
 }
