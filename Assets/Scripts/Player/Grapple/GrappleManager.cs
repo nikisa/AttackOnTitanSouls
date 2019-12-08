4cdabaa730d73141879f110c3ce684c8c00b1643
Nicolò Zannini <nikisa97@gmail.com>
2019-12-08 21:29:09 +0100

    Grappling Manager finished (not tested)

--- Assets/Scripts/Player/Grapple/GrappleManager.cs
+++ Assets/Scripts/Player/Grapple/GrappleManager.cs
@@ -272,6 +272,8 @@ public class GrappleManager : MonoBehaviour
         float SumOfLength = 0;
         float pointsDisatnce = 0;
         float indexNumber;
+        float pointsUnwinded = 0;
+
         Vector3 previousInstance = Vector3.zero;
         RopeNode massPoint;
 
@@ -292,8 +294,8 @@ public class GrappleManager : MonoBehaviour
             massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (1 - DynamicFriction);
 
             previousInstance = massPoint.transform.position;
-            pointsDisatnce = Vector3.Distance(hook.transform.position , massPoint.transform.position);
-            
+            pointsDisatnce = Vector3.Distance(hook.transform.position, massPoint.transform.position);
+
         }
 
         SumOfLength += pointsDisatnce;
@@ -301,7 +303,7 @@ public class GrappleManager : MonoBehaviour
 
         for (int i = 1; i < indexNumber; i++) {
             massPoint = RopeNodes[i] as RopeNode;
-            pointsDisatnce = Vector3.Distance(previousInstance , massPoint.transform.position);
+            pointsDisatnce = Vector3.Distance(previousInstance, massPoint.transform.position);
 
             if (pointsDisatnce > MassPointLength) {
                 error = pointsDisatnce - MassPointLength;
@@ -311,14 +313,181 @@ public class GrappleManager : MonoBehaviour
 
             previousInstance = massPoint.transform.position;
             SumOfLength += pointsDisatnce;
-            pointsDisatnce = Vector3.Distance(previousInstance, GrappleGun.transform.position);
+        }
+        pointsDisatnce = Vector3.Distance(previousInstance, GrappleGun.transform.position);
+
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0) {
+            if (pointsDisatnce > MassPointLength) {
+                error = pointsDisatnce - MassPointLength;
+                changeX = error * Mathf.Cos(Vector3.Angle(previousInstance, GrappleGun.transform.position));
+                changeY = error * Mathf.Sin(Vector3.Angle(previousInstance, GrappleGun.transform.position));
+                Player.transform.position = new Vector3(Player.transform.position.x - changeX * springValue, Player.transform.position.y, Player.transform.position.z - changeY * springValue);
+
+                massPoint = RopeNodes[RopeNodes.Count - 1] as RopeNode;
+                massPoint.transform.position = new Vector3(massPoint.transform.position.x + changeX * (1 - springValue), massPoint.transform.position.y, massPoint.transform.position.z + changeY * (1 - springValue));
+                massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (1 - DynamicFriction);
+                pointsDisatnce = Vector3.Distance(previousInstance, GrappleGun.transform.position);
+                SumOfLength += pointsDisatnce;
+            }
+        }
+        massPoint.gameObject.SetActive(false);
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+            pointsUnwinded = Mathf.Ceil(pointsDistance / MassPointLength) - 1;
+
+            for (int i = 1; i <= pointsUnwinded; i++) {
+                massPoint = RopeNodes[i] as RopeNode;
+
+                pointsDistance = Vector3.Distance(previousInstance, massPoint.transform.position);
+
+                if (pointsDistance > MassPointLength) {
+                    error = pointsDistance - MassPointLength;
+                    changeX = error * Mathf.Cos(Vector3.Angle(previousInstance, massPoint.transform.position));
+                    changeY = error * Mathf.Sin(Vector3.Angle(previousInstance, massPoint.transform.position));
+
+                    massPoint.transform.position = new Vector3(massPoint.transform.position.x - changeX * springValue, massPoint.transform.position.y, massPoint.transform.position.z - changeY * springValue);
+                    massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (1 - DynamicFriction);
+                }
+                previousInstance = massPoint.transform.position;
+                SumOfLength += pointsDisatnce;
+            }
+
+            if (pointsDisatnce > MassPointLength) {
+                error = pointsDistance - MassPointLength;
+                changeX = error * Mathf.Cos(Vector3.Angle(previousInstance, massPoint.transform.position));
+                changeY = error * Mathf.Sin(Vector3.Angle(previousInstance, massPoint.transform.position));
+                springValue = 1;
+                Player.transform.position = new Vector3(Player.transform.position.x - changeX * springValue, Player.transform.position.y, Player.transform.position.z - changeY * springValue);
+                massPoint.gameObject.SetActive(false);
+            }
+
+            if (SumOfLength * (2 - springValue) > RopeNodes.Count * MassPointLength) {
+                hook.ropeFinished = true;
+            }
+
         }
     }
 
-    
-    public void DisableMassPoints() {
+    public void UpdateMassPoint() {
+
+        float springVerlet = 1;
+        float dynamicFriction = 25;
+        float sumOfLength = 0;
+
+        RaycastHit hit;
+        Physics.Raycast(hook.transform.position, hook.transform.forward, out hit, Mathf.Infinity);
+        Debug.DrawRay(hook.transform.position, hook.transform.forward, Color.blue);
+
+        foreach (RopeNode massPoint in RopeNodes) {
+            massPoint.OldPos = massPoint.transform.position;
+            massPoint.transform.position = massPoint.transform.position + massPoint.Inertia;
+            massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (100 - dynamicFriction) / 100;
+        }
+
+        if (hit.transform.gameObject.GetComponent<HookPlaque>()) {
+            hook.Inertia = Vector3.zero;
+            hook.transform.position = hit.transform.position;
+            hook.isHooked = true;
+        }
+
+        if (!hook.isHooked) {
+            hook.OldPos = hook.transform.position;
+            hook.transform.position = hook.transform.position + hook.Inertia;
+            hook.Inertia = hook.transform.position - hook.OldPos;
+        }
+    }
+
+    public void HookFired() {
+        UpdatePoints();
+        UpdateLinks();
+        UpdateMassPoint();
+    }
+
+    public void Rewind() {
+        RewindPoints();
+    }
+
+    public void HookFailed() {
+        UpdateMassPointReverse();
+        UpdateLinksReverse();
+    }
+
+    void UpdateMassPointReverse() {
+        float dynamicFriction = 60;
+        foreach (RopeNode massPoint in RopeNodes) {
+            massPoint.OldPos = massPoint.transform.position;
+            massPoint.transform.position = massPoint.transform.position + massPoint.Inertia;
+            massPoint.Inertia = massPoint.transform.position - massPoint.OldPos * (100 - dynamicFriction) / 100;
+        }
+    }
+
+    void UpdateLinksReverse() {
+        float springVerlet = 1;
+        float dynamicFriction = 60;
+        float massPointsDistance = 0;
+        float error = 0;
+        float changeX = 0;
+        float changeY = 0;
+        float provisional = 64; //distance between Player and GrappleGun
+        Vector3 previousInstance;
+
+        RopeNode massPoint = RopeNodes[(int) MassPointNumber - 1] as RopeNode;
+
+        massPointsDistance = Vector3.Distance(GrappleGun.transform.position, massPoint.transform.position);
+
+        if (massPointsDistance > 0) {
+            error = massPointsDistance;
+            changeX = error * Mathf.Cos(Vector3.Angle(GrappleGun.transform.position, massPoint.transform.position)+360);
+            changeY = error * Mathf.Sin(Vector3.Angle(GrappleGun.transform.position, massPoint.transform.position)+360);
+
+            massPoint.transform.position = new Vector3(massPoint.transform.position.x - changeX, massPoint.transform.position.y, massPoint.transform.position.z - changeY);
+            massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (100 - dynamicFriction) / 100;
+        }
+        previousInstance = massPoint.transform.position;
+
+
+        for (int i = 1; i < MassPointNumber; i++) {
+            massPoint = RopeNodes[(int) MassPointNumber-1-i] as RopeNode;
+            massPointsDistance = Vector3.Distance(previousInstance, massPoint.transform.position);
+            if (massPointsDistance > MassPointLength) {
+                error = massPointsDistance - MassPointLength;
+                changeX = error * Mathf.Cos(Vector3.Angle(previousInstance, massPoint.transform.position) + 360);
+                changeY = error * Mathf.Sin(Vector3.Angle(previousInstance, massPoint.transform.position) + 360);
+                massPoint.transform.position = new Vector3(massPoint.transform.position.x - changeX * springVerlet , massPoint.transform.position.y , massPoint.transform.position.z - changeY * springVerlet);
+                massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (100-dynamicFriction)/100;
+            }
+            previousInstance = massPoint.transform.position;
+
+            if (massPointsDistance > MassPointLength) {
+                massPoint = RopeNodes[(int) MassPointNumber-i] as RopeNode;
+                massPoint.transform.position = new Vector3(massPoint.transform.position.x + changeX * (1 - springVerlet), massPoint.transform.position.y, massPoint.transform.position.z + changeY * (1 - springVerlet));
+                massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (100 - dynamicFriction) / 100;
+            }
+
+            massPointsDistance = Vector3.Distance(previousInstance, hook.transform.position);
+
+            if (massPointsDistance > MassPointLength) {
+                error = massPointsDistance - MassPointLength;
+                changeX = error * Mathf.Cos(Vector3.Angle(previousInstance, hook.transform.position) + 360);
+                changeY = error * Mathf.Sin(Vector3.Angle(previousInstance, hook.transform.position) + 360);
+                hook.transform.position = new Vector3(hook.transform.position.x - changeX * springVerlet , hook.transform.position.y , hook.transform.position.z - changeY * springVerlet);
+                hook.Inertia = hook.transform.position - hook.OldPos * (100 - dynamicFriction) / 100;
+
+                massPoint = RopeNodes[0] as RopeNode;
+                massPoint.transform.position = new Vector3(massPoint.transform.position.x - changeX * springVerlet, massPoint.transform.position.y, massPoint.transform.position.z - changeY * springVerlet);
+                massPoint.Inertia = (massPoint.transform.position - massPoint.OldPos) * (100 - dynamicFriction) / 100;
+            }
+
+        }
 
     }
 
+    public void DisableMassPoints() {
+        foreach (RopeNode massPoint in RopeNodes) {
+            //if (masspoint.Id > fromIndex) {
+            massPoint.gameObject.SetActive(false);
+            //}
+        }
+    }
 
 }
