d83cda3fbe858cae03b39a6d6eb9bbd584154b69
Nicolò Zannini <nikisa97@gmail.com>
2019-12-15 21:37:34 +0100

    Dash Frame Perfect + TimeFreeze added

---
+++ Assets/Scripts/Player/Player StateMachine/PlayerDashFramePerfectState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDashFramePerfectState : PlayerBaseState {
+
+    [HideInInspector]
+    public PlayerDashData playerDashData;
+    public float frames;
+
+    //Private
+    float startTime;
+
+    public override void Enter() {
+        playerDashData = player.playerDashData;
+        startTime = Time.time;
+        
+    }
+    public override void Tick() {
+        if ((Time.time - startTime) < frames / 60) {
+            if (player.dataInput.Dash) {
+                Debug.Log("PERFECT TIME");
+                animator.SetTrigger(DASH);
+            }
+        }
+        else {
+            animator.SetTrigger(IDLE);
+        }
+    }
+
+    public override void Exit() {
+
+    }
+
+   
+}
