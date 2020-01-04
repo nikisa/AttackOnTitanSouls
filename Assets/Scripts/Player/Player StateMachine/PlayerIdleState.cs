09295dcc25b76d916d1da6a225436a9f6c4e1ec1
Nicolò Zannini <nikisa97@gmail.com>
2020-01-04 19:31:17 +0100

    PlayerController refactoring

--- Assets/Scripts/Player/Player StateMachine/PlayerIdleState.cs
+++ Assets/Scripts/Player/Player StateMachine/PlayerIdleState.cs
@@ -11,83 +11,20 @@ public class PlayerIdleState : PlayerBaseState {
 
 
     // Private
-    bool newInput;
     bool canDash = true;    
-    float accelRatePerSec;
-    float decelRatePerSec;
-    float forwardVelocity;
     float startDash;
     float startTime;
     float boostTime;
     float timer;
-    int layerMask;
+    float accelRatePerSec;
+    float decelRatePerSec;
+    float forwardVelocity;
     Vector3 movementVelocity = Vector3.zero;
-    //InputData inputData;
     DataInput dataInput;
-
-    
-    public void ReadInputKeyboard(DataInput dataInput) {
-
-        movementVelocity = Vector3.zero;
-
-        if (!playerIdleData.isHookTest) {
-            // Set vertical movement
-            if (dataInput.Vertical != 0f) {
-                forwardVelocity += accelRatePerSec * Time.deltaTime;
-                forwardVelocity = Mathf.Clamp(forwardVelocity, 0, playerIdleData.maxSpeed);
-                movementVelocity += Vector3.forward * dataInput.Vertical * forwardVelocity;
-            }
-
-            // Set horizontal movement
-            if (dataInput.Horizontal != 0f) {
-                forwardVelocity += accelRatePerSec * Time.deltaTime;
-                forwardVelocity = Mathf.Clamp(forwardVelocity, 0, playerIdleData.maxSpeed);
-                movementVelocity += Vector3.right * dataInput.Horizontal * forwardVelocity;
-            }
-        }
-        else {
-            // Set vertical movement
-            if (dataInput.Vertical != 0f) {
-                forwardVelocity += accelRatePerSec * Time.deltaTime;
-                forwardVelocity = Mathf.Clamp(forwardVelocity, 0, playerIdleData.maxSpeed);
-                movementVelocity += Vector3.up * dataInput.Vertical * forwardVelocity;
-            }
-
-            // Set horizontal movement
-            if (dataInput.Horizontal != 0f) {
-                forwardVelocity += accelRatePerSec * Time.deltaTime;
-                forwardVelocity = Mathf.Clamp(forwardVelocity, 0, playerIdleData.maxSpeed);
-                movementVelocity += Vector3.right * dataInput.Horizontal * forwardVelocity;
-            }
-        }
-        
-        newInput = true;
-    }
-
-    public void ReadInputGamepad(DataInput dataInput) {
-
-        movementVelocity = Vector3.zero;
-
-        // Set vertical movement gamepad
-        if (dataInput.Vertical != 0f) {
-            forwardVelocity += accelRatePerSec * Time.deltaTime;
-            forwardVelocity = Mathf.Clamp(forwardVelocity, 0, playerIdleData.maxSpeed);
-            movementVelocity += Vector3.forward * dataInput.Vertical * forwardVelocity;
-        }
-
-        // Set horizontal movement gamepad
-        if (dataInput.Horizontal != 0f) {
-            forwardVelocity += accelRatePerSec * Time.deltaTime;
-            forwardVelocity = Mathf.Clamp(forwardVelocity, 0, playerIdleData.maxSpeed);
-            movementVelocity += Vector3.right * dataInput.Horizontal * forwardVelocity;
-        }
-
-        newInput = true;
-    }
 
 
     public override void Enter() {
-        layerMask = 1 << 10 | 1<<12;
+        player.layerMask = 1 << 10 | 1<<12;
         timer = Time.time;
 
         accelRatePerSec = playerIdleData.maxSpeed / (playerIdleData.framesZeroToMax / 60);
@@ -97,7 +34,7 @@ public class PlayerIdleState : PlayerBaseState {
 
     public override void Tick() {
 
-        //Debug.LogFormat("Horizontal: {0} ||| Vertical: {1}", player.dataInput.Horizontal, player.dataInput.Vertical);
+        player.PlayerInclination();
 
         if (Time.time < timer + playerIdleData.resumeControl) {
             canDash = false;
@@ -107,13 +44,6 @@ public class PlayerIdleState : PlayerBaseState {
         }
 
         dataInput = player.dataInput;
-
-        player.rotationTransform.localRotation = dataInput.currentOrientation;
-
-        // ruoto il personaggio in funzione della del suo movimento
-        Vector3 rotationAxis = Quaternion.AngleAxis(90,Vector3.up)* movementVelocity;
-        Quaternion moveRotation = Quaternion.AngleAxis(movementVelocity.magnitude * player.movimentRatio, rotationAxis);
-        player.body.rotation = moveRotation * player.rotationTransform.rotation;
 
         if (dataInput.Dash && canDash)
         {
@@ -127,136 +57,22 @@ public class PlayerIdleState : PlayerBaseState {
         }
 
         if (dataInput.Vertical != 0 || dataInput.Horizontal != 0) {
-            ReadInputGamepad(dataInput);
-            ReadInputKeyboard(dataInput);
+            //player.ReadInputGamepad(dataInput , accelRatePerSec);
+            player.ReadInputKeyboard(dataInput , accelRatePerSec , playerIdleData.maxSpeed);
         }
 
-        if (newInput) {
-            Movement();
+        if (player.newInput) {
+            player.Movement();
         }
         else {
-            Deceleration();
+            player.Deceleration(decelRatePerSec);
         }
 
-        newInput = false;
+        player.newInput = false;
     }
 
     public override void Exit() {
         
-    }
-
-    public void Deceleration() {
-        forwardVelocity = 0;
-
-        if (movementVelocity.x < decelRatePerSec * Time.deltaTime)
-            movementVelocity.x = movementVelocity.x - decelRatePerSec * Time.deltaTime;
-        else if (movementVelocity.x > -decelRatePerSec * Time.deltaTime)
-            movementVelocity.x = movementVelocity.x + decelRatePerSec * Time.deltaTime;
-        else {
-            movementVelocity.x = 0;
-        }
-
-        if (!playerIdleData.isHookTest) {
-            if (movementVelocity.z < decelRatePerSec * Time.deltaTime)
-                movementVelocity.z = movementVelocity.z - decelRatePerSec * Time.deltaTime;
-            else if (movementVelocity.z > -decelRatePerSec * Time.deltaTime)
-                movementVelocity.z = movementVelocity.z + decelRatePerSec * Time.deltaTime;
-            else {
-                movementVelocity.z = 0;
-            }
-
-
-            Movement();
-        }
-        else {
-            if (movementVelocity.y < decelRatePerSec * Time.deltaTime)
-                movementVelocity.y = movementVelocity.z - decelRatePerSec * Time.deltaTime;
-            else if (movementVelocity.y > -decelRatePerSec * Time.deltaTime)
-                movementVelocity.y = movementVelocity.z + decelRatePerSec * Time.deltaTime;
-            else {
-                movementVelocity.y = 0;
-            }
-
-            Movement();
-
-        }
-
-    }
-
-    public void Movement() 
-    {
-        float skin = .5f;
-        float softSkin = 0.01f;
-
-        if (movementVelocity.sqrMagnitude < 0.001) return;
-
-        int interpolation = (int)(movementVelocity.magnitude/1f) +1;
-
-        for (int i = 0; i < interpolation; i++)
-        {
-            if (movementVelocity.sqrMagnitude < 0.001) return;
-
-            float time = Time.deltaTime / interpolation;
-
-            RaycastHit[] hits = Physics.SphereCastAll(player.transform.position + Vector3.up * 1.1f, skin, movementVelocity, (movementVelocity * time).magnitude , layerMask);
-
-            if (hits == null || hits.Length == 0) {
-                player.transform.Translate(movementVelocity * time);
-            }
-        else {
-                //pushable
-                foreach (var hit in hits) {
-                    if (hit.transform.gameObject.CompareTag("Pushable")) {
-                        var rb = hit.transform.GetComponent<Rigidbody>();
-                        rb.AddForceAtPosition(movementVelocity, hit.point, ForceMode.Acceleration);
-                        //Debug.LogFormat("Impact:{0}", movementVelocity);
-                    }
-                    else if (hits[0].transform.gameObject.CompareTag("BossGraphics")) {
-                        Debug.Log("CAVOLFIORE");
-                        SceneManager.LoadScene(2);
-
-                    }
-
-                    //slope
-                    if (hits.Length == 1) {
-                        var normal = Quaternion.AngleAxis(90, Vector3.up) * hits[0].normal;
-                        Debug.DrawRay(hits[0].point, normal * 4, Color.red, 2);
-
-                        movementVelocity = normal * Vector3.Dot(movementVelocity, normal);
-                        movementVelocity.y = 0;
-
-                        player.transform.Translate(movementVelocity * time);
-                        //Verlet movement
-                    }
-
-                }
-
-            }
-
-                //player.transform.Translate(movementVelocity.normalized * (hit.distance- softSkin));
-                //Debug.Log(hit.transform.gameObject.name, hit.transform.gameObject);
-           
-        }
-
-
-        
-        //player.controller.Move(movementVelocity * Time.deltaTime + Vector3.down);
-
-        /*
-        if (!playerIdleData.isHookTest) {
-            player.transform.Translate(movementVelocity * Time.deltaTime);
-        }
-        else {
-            player.transform.Translate(movementVelocity * Time.deltaTime);
-        }
-        */
-    }
-
-
-    //?Test player rope length constraint?
-    public void RopeLengthConstraint(Vector3 nodePosition) {
-        Vector3 constraint = new Vector3(nodePosition.x, nodePosition.y, player.transform.position.z);
-        player.transform.Translate(constraint * Time.deltaTime);
     }
 }
 
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngineInternal.Input;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerController : MonoBehaviour
@@ -19,6 +20,8 @@ public class PlayerController : MonoBehaviour
 
     //Public
     [HideInInspector]
+    public bool newInput;
+    [HideInInspector]
     public PlayerDashData playerDashData;
     [HideInInspector]
     public PlayerIdleData playerIdleData;
@@ -26,24 +29,22 @@ public class PlayerController : MonoBehaviour
     public Vector3 dashDirection;
     [HideInInspector]
     public float InitialDashVelocity;
+    [HideInInspector]
+    public int layerMask;
 
     //Private
     Camera camera;
-    bool useController;
+    Vector3 movementVelocity = Vector3.zero;
+    float forwardVelocity;
+    
 
     protected virtual void Start() {
         camera = Camera.main;
-
+        
         foreach (var item in animator.GetBehaviours<BaseState>()) {
             item.SetContext(this, animator);
         }
     }
-
-    // Set Dash Action
-    //    if (data.buttons[0] && isMoving) {
-    //        Dash(DashTimeFrames, ResumeControl, DashTimeFreeze, data);
-    //        isMoving = true;
-    //    }
 
     private void Update() 
     {
@@ -64,9 +65,6 @@ public class PlayerController : MonoBehaviour
         if (direction.sqrMagnitude > 0.001f) dataInput.currentOrientation = Quaternion.LookRotation(direction.normalized);
     }
 
-    //public InputData getData() {
-    //    return im.data;
-    //}
 
     public void CheckInput() {
         dataInput.Horizontal = Input.GetAxis("Horizontal");
@@ -75,11 +73,7 @@ public class PlayerController : MonoBehaviour
         dataInput.VerticalLook = Input.GetAxis("VerticalLook");
         dataInput.Dash = Input.GetButtonDown("Dash");
 
-
-        //Vector3 lookVector = new Vector3(dataInput.HorizontalLook, 0, dataInput.VerticalLook);
         Vector3 lookVector = Vector3.right * dataInput.HorizontalLook + Vector3.forward * dataInput.VerticalLook;
-
-
 
         if (lookVector.sqrMagnitude < 0.0001f && Input.GetJoystickNames().Length<=0)
         {
@@ -103,18 +97,147 @@ public class PlayerController : MonoBehaviour
         Time.timeScale = originalScale;
     }
 
+
+
+    public void Movement() {
+        float skin = .95f;
+
+        if (movementVelocity.sqrMagnitude < 0.001) return;
+
+        int interpolation = (int)(movementVelocity.magnitude / 1f) + 1;
+
+        for (int i = 0; i < interpolation; i++) {
+            if (movementVelocity.sqrMagnitude < 0.001) return;
+
+            float time = Time.deltaTime / interpolation;
+
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position + Vector3.up * 1.5f, skin, movementVelocity, (movementVelocity * time).magnitude, layerMask);
+
+            if (hits == null || hits.Length == 0) {
+                transform.Translate(movementVelocity * time);
+            }
+            else {
+                //pushable
+                foreach (var hit in hits) {
+                    if (hit.transform.gameObject.CompareTag("Pushable")) {
+                        var rb = hit.transform.GetComponent<Rigidbody>();
+                        rb.AddForceAtPosition(movementVelocity, hit.point, ForceMode.Acceleration);
+                    }
+                    else if (hits[0].transform.gameObject.CompareTag("BossGraphics")) {
+                        SceneManager.LoadScene(2);
+                    }
+
+                    //slope
+                    if (hits.Length == 1) {
+                        var normal = Quaternion.AngleAxis(90, Vector3.up) * hits[0].normal;
+                        Debug.DrawRay(hits[0].point, normal * 4, Color.red, 2);
+
+                        movementVelocity = normal * Vector3.Dot(movementVelocity, normal);
+                        movementVelocity.y = 0;
+
+                        transform.Translate(movementVelocity * time);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void Deceleration(float _deceleration) {
+        forwardVelocity = 0;
+
+        if (movementVelocity.x < _deceleration * Time.deltaTime)
+            movementVelocity.x = movementVelocity.x - _deceleration * Time.deltaTime;
+        else if (movementVelocity.x > -_deceleration * Time.deltaTime)
+            movementVelocity.x = movementVelocity.x + _deceleration * Time.deltaTime;
+        else {
+            movementVelocity.x = 0;
+        }
+            if (movementVelocity.z < _deceleration * Time.deltaTime)
+                movementVelocity.z = movementVelocity.z - _deceleration * Time.deltaTime;
+            else if (movementVelocity.z > -_deceleration * Time.deltaTime)
+                movementVelocity.z = movementVelocity.z + _deceleration * Time.deltaTime;
+            else {
+                movementVelocity.z = 0;
+            }
+            Movement();
+    }
+
+    public void ReadInputKeyboard(DataInput dataInput , float _acceleration , float _maxSpeed) {
+
+    movementVelocity = Vector3.zero;
+    // Set vertical movement
+    if (dataInput.Vertical != 0f) {
+        forwardVelocity += _acceleration * Time.deltaTime;
+        forwardVelocity = Mathf.Clamp(forwardVelocity, 0, _maxSpeed);
+        movementVelocity += Vector3.forward * dataInput.Vertical * forwardVelocity;
+    }
+
+    // Set horizontal movement
+    if (dataInput.Horizontal != 0f) {
+        forwardVelocity += _acceleration * Time.deltaTime;
+        forwardVelocity = Mathf.Clamp(forwardVelocity, 0, _maxSpeed);
+        movementVelocity += Vector3.right * dataInput.Horizontal * forwardVelocity;
+    }
+        
+    newInput = true;
+    
 }
 
-    public struct DataInput {
-        public float Horizontal;
-        public float HorizontalLook;
-        public float Vertical;
-        public float VerticalLook;
-        public bool Dash;
-        public Quaternion currentOrientation;
+
+    public void ReadInputGamepad(DataInput dataInput, float _acceleration , float _maxSpeed) {
+
+        movementVelocity = Vector3.zero;
+
+        // Set vertical movement gamepad
+        if (dataInput.Vertical != 0f) {
+            forwardVelocity += _acceleration * Time.deltaTime;
+            forwardVelocity = Mathf.Clamp(forwardVelocity, 0, _maxSpeed);
+            movementVelocity += Vector3.forward * dataInput.Vertical * forwardVelocity;
+        }
+
+        // Set horizontal movement gamepad
+        if (dataInput.Horizontal != 0f) {
+            forwardVelocity += _acceleration * Time.deltaTime;
+            forwardVelocity = Mathf.Clamp(forwardVelocity, 0, _maxSpeed);
+            movementVelocity += Vector3.right * dataInput.Horizontal * forwardVelocity;
+        }
+        newInput = true;
+    }
 
 
-    public DataInput(float _horizontal, float _vertical , bool _dash , Quaternion _currentRotation) {
+
+    public void PlayerInclination() {
+
+        rotationTransform.localRotation = dataInput.currentOrientation;
+
+        // Ruoto il personaggio in funzione della del suo movimento
+        Vector3 rotationAxis = Quaternion.AngleAxis(90, Vector3.up) * movementVelocity;
+        Quaternion moveRotation = Quaternion.AngleAxis(movementVelocity.magnitude * movimentRatio, rotationAxis);
+        body.rotation = moveRotation * rotationTransform.rotation;
+    }
+
+
+    //?Test player rope length constraint?
+    public void RopeLengthConstraint(Vector3 nodePosition) {
+        Vector3 constraint = new Vector3(nodePosition.x, nodePosition.y, transform.position.z);
+        transform.Translate(constraint * Time.deltaTime);
+    }
+
+
+}
+
+public struct DataInput
+{
+    public float Horizontal;
+    public float HorizontalLook;
+    public float Vertical;
+    public float VerticalLook;
+    public bool Dash;
+    public Quaternion currentOrientation;
+
+
+    public DataInput(float _horizontal, float _vertical, bool _dash, Quaternion _currentRotation) {
         Horizontal = _horizontal;
         Vertical = _vertical;
         Dash = _dash;
@@ -122,5 +245,7 @@ public class PlayerController : MonoBehaviour
         HorizontalLook = 0;
         VerticalLook = 0;
     }
-    
+
+   
 }
+
